b8d994f94b916636f9378b82c1b845dd749bacaa
ivanushka13 <vano.davydov1997@mail.ru>
2025-08-05 13:56:34 +0300

    init commit

---
+++ src/test/java/ru/davydov/eventmanger/EventMangerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.davydov.eventmanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventMangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
